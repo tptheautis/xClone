bdea230057d184566c8297a0835121189456e27d
Theautis Persons <tptheautis@gmail.com>
2023-09-30 12:00:07 -0700

    right sidebar imported

--- src/rightSidebar.js
+++ src/rightSidebar.js
@@ -1,5 +1,68 @@
+import { happeningContainer, column3, searchBar, whoToFollowContainer } from './index';
+
 function createRightSidebar() {
-  console.log('right');
+  const searchTwitter = document.createElement('div');
+  const whatsHappening = document.createElement('h2');
+  const searchTwitterText = document.createTextNode('Search Twitter');
+  const whatsHappeningText = document.createTextNode('What\'s Happening');
+  searchTwitter.append(searchTwitterText);
+  happeningContainer.append(whatsHappeningText);
+  column3.append(searchTwitter);
+  column3.append(whatsHappening);
+
+  // Right Sidebar
+  const whoToFollowText = document.createTextNode('Who to follow');
+  // SearchBar
+  const searchIcon = document.createElement('img');
+  searchIcon.classList.add('searchIcon');
+  searchIcon.src = '../src/assets/searchIcon.png';
+  searchBar.append(searchIcon);
+  searchBar.append(searchTwitterText);
+  // Odin Project Profile
+  const odinContainer = document.createElement('div');
+  odinContainer.classList.add('odinContainer');
+  const odinIcon = document.createElement('img');
+  odinIcon.classList.add('odinIcon');
+  odinIcon.src = '../src/assets/odinIcon.png';
+  const odinTextContainer = document.createElement('div');
+  odinTextContainer.classList.add('whoToFollowContainerChild');
+  const odinName = document.createTextNode('The Odin Project');
+  const odinUsername = document.createTextNode('@TheOdinProject');
+  whoToFollowContainer.append(whoToFollowText);
+  whoToFollowContainer.append(odinContainer);
+  odinContainer.append(odinIcon);
+  odinContainer.append(odinName);
+  odinContainer.append(odinUsername);
+  // Stack Overflow Profile
+  const stackContainer = document.createElement('div');
+  stackContainer.classList.add('stackContainer');
+  const stackIcon = document.createElement('img');
+  stackIcon.classList.add('stackIcon');
+  stackIcon.src = '../src/assets/stackIcon.png';
+  const stackName = document.createTextNode('Stack Overflow');
+  const stackUsername = document.createTextNode('@StackOverflow');
+  whoToFollowContainer.append(stackContainer);
+  stackContainer.append(stackIcon);
+  stackContainer.append(stackName);
+  stackContainer.append(stackUsername);
+  // Github Profile
+  const githubContainer = document.createElement('div');
+  githubContainer.classList.add('githubContainer');
+  const githubIcon = document.createElement('img');
+  githubIcon.classList.add('githubIcon');
+  githubIcon.src = '../src/assets/gitIcon.png';
+  const githubName = document.createTextNode('GitHub');
+  const githubUsername = document.createTextNode('@github');
+  whoToFollowContainer.append(githubContainer);
+  githubContainer.append(githubIcon);
+  githubContainer.append(githubName);
+  githubContainer.append(githubUsername);
+  // Show More
+  const showMoreContainer = document.createElement('div');
+  showMoreContainer.classList.add('showMoreContainer');
+  const showMoreText = document.createTextNode('Show more');
+  whoToFollowContainer.append(showMoreContainer);
+  showMoreContainer.append(showMoreText);
 }
 
 export default createRightSidebar;

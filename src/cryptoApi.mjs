60f1d9f72aa3e60f5e16b423e818719eae4f4e9c
Theautis Persons <tptheautis@gmail.com>
2024-04-25 00:59:07 -0700

    added ETH and XRP API pairs

--- src/cryptoApi.mjs
+++ src/cryptoApi.mjs
@@ -5,10 +5,28 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 fetch('https://rest.coinapi.io/v1/exchangerate/BTC/USD', {
-headers: {
-    "X-CoinAPI-Key": process.env.cryptoPriceApiKey,
-    }
+  headers: {
+    'X-CoinAPI-Key': process.env.cryptoPriceApiKey,
+  },
 })
-.then(response => response.json())
-.then(data => console.log(data))
-.catch(error => console.error('Error:', error));
+  .then((response) => response.json())
+  .then((data) => console.log(data))
+  .catch((error) => console.error('Error:', error));
+
+fetch('https://rest.coinapi.io/v1/exchangerate/ETH/USD', {
+  headers: {
+    'X-CoinAPI-Key': process.env.cryptoPriceApiKey,
+  },
+})
+  .then((response) => response.json())
+  .then((data) => console.log(data))
+  .catch((error) => console.error('Error:', error));
+
+fetch('https://rest.coinapi.io/v1/exchangerate/XRP/USD', {
+  headers: {
+    'X-CoinAPI-Key': process.env.cryptoPriceApiKey,
+  },
+})
+  .then((response) => response.json())
+  .then((data) => console.log(data))
+  .catch((error) => console.error('Error:', error));
